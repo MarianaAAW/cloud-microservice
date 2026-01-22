a62e4fbf89517756718cad48038d41c6eb9dc18f
MarianaAAW <aamariana1907@gmail.com>
2026-01-22 05:00:21 +0100

    updating flask

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask, jsonify
+import os
+from datetime import datetime, timezone
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify({
+    current_info = {
         "service": "cloud-microservice",
         "status": "running",
-        "version": "1.0"
-    })
+        "version": "1.0.0",
+        "timestamp_utc": datetime.now(timezone.utc).isoformat(),
+        "environment": os.getenv("ENV", "development"),
+        "host": socket.gethostname()
+    }
+   
+    return jsonify(current_info)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
